26c02ac06ba31e395609b5a9b76aec1c555960f4
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-11-11 18:03:59 +0530

    initial drawing buffer set

---
+++ simple-triangle/app.js
@@ -0,0 +1,11 @@
+var vertexShaderText = [].join("\n");
+
+var initDemo = function () {
+  console.log("working");
+
+  const canvas = document.getElementById("game-surface");
+  const gl = canvas.getContext("webgl");
+  console.log(gl);
+  gl.clearColor(0.75, 0.85, 0.8, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+};
