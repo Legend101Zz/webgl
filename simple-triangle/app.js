b1cb9e716d6835f8404b20232cd23205bebc4a36
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-11-11 18:14:44 +0530

    added glsl code

--- simple-triangle/app.js
+++ simple-triangle/app.js
@@ -1,4 +1,22 @@
-var vertexShaderText = [].join("\n");
+var vertexShaderText = [
+  "presion mediump float;",
+  "",
+  "attribute vec2 vertPosition;",
+  "",
+  "void main()",
+  "{",
+  " gl_Position = vec4(vertPosition,0.0,1.0);",
+  "}",
+].join("\n");
+
+var fragmentShaderText = [
+  "presion mediump float;",
+  "",
+  "void main()",
+  "{",
+  " gl_Fragcolor = vec4(1.0,0.0,0.0,1.0);",
+  "}",
+].join("\n");
 
 var initDemo = function () {
   console.log("working");
@@ -8,4 +26,10 @@ var initDemo = function () {
   console.log(gl);
   gl.clearColor(0.75, 0.85, 0.8, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  //
+  //shaders
+  //
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 };
